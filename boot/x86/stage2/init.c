f7c494456621122e446d3cb9518d44d8895b6f06
Morten Delenk <mtg.morten@googlemail.com>
2016-07-30 12:11:04 +0200

    Before rewrite

---
+++ boot/x86/stage2/init.c
@@ -0,0 +1,13 @@
+void putstring(short);
+void main() {
+    putstring((short)"Hello, C!\r\n");
+    putstring((short)"Goodbye, C!\r\n");
+    putstring((short)"Hello, C!\r\n");
+    putstring((short)"Goodbye, C!\r\n");
+    putstring((short)"Hello, C!\r\n");
+    putstring((short)"Goodbye, C!\r\n");
+    putstring((short)"Hello, C!\r\n");
+    putstring((short)"Goodbye, C!\r\n");
+    putstring((short)"Hello, C!\r\n");
+    putstring((short)"Goodbye, C!\r\n");
+}
